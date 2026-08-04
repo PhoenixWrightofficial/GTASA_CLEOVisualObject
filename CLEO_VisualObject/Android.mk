81ed370e66f73d32410b1b693f50df1f05a8cf88
PhoenixWrightofficial <xxmidasazer@gmail.com>
2026-08-04 20:20:31 +0500

    Update Android.mk

--- CLEO_VisualObject/Android.mk
+++ CLEO_VisualObject/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_CPP_EXTENSION := .cpp .cc
 LOCAL_MODULE := CLEO_VisualObject
 LOCAL_SRC_FILES := main.cpp mod/logger.cpp
-LOCAL_CFLAGS += -O2 -mfloat-abi=softfp -DNDEBUG -std=c++17
+LOCAL_CFLAGS += -O2 -DNDEBUG -std=c++17
 LOCAL_LDLIBS += -llog
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/rw
 
